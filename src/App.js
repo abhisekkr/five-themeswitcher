1e0d699d7f87983beed1704a0ee7f40565a1ad85
abhisekkr98 <60818509+abhisekkr98@users.noreply.github.com>
2021-01-04 19:59:46 +0530

    Update App.js

--- src/App.js
+++ src/App.js
@@ -1,11 +1,11 @@
-import React,{useState} from "react";     //here we are setting the default theme at the time of loading the application by useState
+import React,{useState} from "react";     
 
 import ThemeContext from "./Context/ThemeContext";
 import Header from "./Components/Header";
 import HeroSection from "./Components/HeroSection";
 
 const App = () => {
-  const themeHook= useState("light")                  //light will be the default at time of loading application
+  const themeHook= useState("light")                  
   return(
     <ThemeContext.Provider value={themeHook}>
         <div>
@@ -19,5 +19,3 @@ const App = () => {
 
 export default App;
 
-//here the ThemeContext is the provider
-//when we want to consume what provider is providing then we wrap it into the consumer
